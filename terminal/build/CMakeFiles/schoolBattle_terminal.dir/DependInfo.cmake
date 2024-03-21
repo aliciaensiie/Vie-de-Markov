c3339adbb54722f1139032c5cae593a1a8cfa234
aliciaensiie <alicia.plath62@orange.fr>
2024-03-21 13:15:34 +1300

    cmakelist terminal

---
+++ terminal/build/CMakeFiles/schoolBattle_terminal.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/cartes.c" "CMakeFiles/schoolBattle_terminal.dir/src/cartes.c.o" "gcc" "CMakeFiles/schoolBattle_terminal.dir/src/cartes.c.o.d"
+  "/home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/effets.c" "CMakeFiles/schoolBattle_terminal.dir/src/effets.c.o" "gcc" "CMakeFiles/schoolBattle_terminal.dir/src/effets.c.o.d"
+  "/home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/interface.c" "CMakeFiles/schoolBattle_terminal.dir/src/interface.c.o" "gcc" "CMakeFiles/schoolBattle_terminal.dir/src/interface.c.o.d"
+  "/home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/joueuses.c" "CMakeFiles/schoolBattle_terminal.dir/src/joueuses.c.o" "gcc" "CMakeFiles/schoolBattle_terminal.dir/src/joueuses.c.o.d"
+  "/home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/main.c" "CMakeFiles/schoolBattle_terminal.dir/src/main.c.o" "gcc" "CMakeFiles/schoolBattle_terminal.dir/src/main.c.o.d"
+  "/home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/personnages.c" "CMakeFiles/schoolBattle_terminal.dir/src/personnages.c.o" "gcc" "CMakeFiles/schoolBattle_terminal.dir/src/personnages.c.o.d"
+  "/home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/structures.c" "CMakeFiles/schoolBattle_terminal.dir/src/structures.c.o" "gcc" "CMakeFiles/schoolBattle_terminal.dir/src/structures.c.o.d"
+  "/home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/zones.c" "CMakeFiles/schoolBattle_terminal.dir/src/zones.c.o" "gcc" "CMakeFiles/schoolBattle_terminal.dir/src/zones.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ terminal/build/CMakeFiles/schoolBattle_terminal.dir/src/personnages.c.o.d
@@ -0,0 +1,41 @@
+CMakeFiles/schoolBattle_terminal.dir/src/personnages.c.o: \
+ /home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/personnages.c \
+ /usr/include/stdc-predef.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_tm.h \
+ /home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/../headers/personnages.h \
+ /home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/../headers/zones.h \
+ /home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/../headers/joueuses.h \
+ /home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/../headers/cartes.h \
+ /home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/../headers/interface.h \
+ /home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/../headers/cartes.h \
+ /home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/../headers/joueuses.h \
+ /home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/../headers/effets.h \
+ /home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/../headers/zones.h \
+ /home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/../headers/structures.h \
+ /home/arry/Documents/projetc++/Vie-de-Markov/terminal/src/../headers/personnages.h
